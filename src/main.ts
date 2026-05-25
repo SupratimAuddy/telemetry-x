bdce40dd45d35a8079eb1b85e6fe792a1efe08f8
Supratim Auddy <supratimauddy.99@hotmail.com>
2026-05-25 22:33:36 +0530

    [sa11051997-0] feat: update dependencies and enhance styling with Angular Material theming

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
